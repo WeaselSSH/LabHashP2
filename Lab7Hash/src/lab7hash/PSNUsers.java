e3448cfcb76bc10819c743d55577ba0f34973398
DJCC <david.carcamo2006@gmail.com>
2025-12-05 09:09:38 -0600

    creacion de reloadHashTable

--- Lab7Hash/src/lab7hash/PSNUsers.java
+++ Lab7Hash/src/lab7hash/PSNUsers.java
@@ -4,6 +4,7 @@
  */
 package lab7hash;
 
+import java.io.IOException;
 import java.io.RandomAccessFile;
 
 /**
@@ -11,14 +12,42 @@ import java.io.RandomAccessFile;
  * @author David
  */
 public class PSNUsers {
-    public static RandomAccessFile archivoPSN;
-    public static HashTable lista;
+    public RandomAccessFile archivoPSN;
+    private HashTable lista;
+    
     public PSNUsers(RandomAccessFile archivoPSN, HashTable lista){
         this.archivoPSN=archivoPSN;
         this.lista=lista;
         
-        //reloadHastTable();
+        try{
+            reloadHashTable();
+        }catch(IOException e){};
+        
     }
     
+    
+    
+    
+    /*
+    
+    */
+    private void reloadHashTable() throws IOException{
+        //chequeo de usuarios dentro del archivo binario
+        archivoPSN.seek(0);
+        long pos;
+        while(archivoPSN.getFilePointer()< archivoPSN.length()){
+            pos =archivoPSN.getFilePointer();//posicion de donde empiezan los datos de X usuario
+            
+            archivoPSN.seek(pos);//volvemos a la posicion para ahora sacar los datos
+            String username = archivoPSN.readUTF();//read name
+            archivoPSN.readInt();//read trofeos
+            archivoPSN.readInt();//read puntos
+            
+             //agregar al hastable
+            if(archivoPSN.readBoolean()==true){
+                lista.add(username, pos);//agrega usuario si este esta activo
+            }
+        }
+    }
     
 }
